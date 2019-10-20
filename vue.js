982004820f587c0b94dd45d07da3ccfc71dc5c83
sundayifeanyi <42241014+sundayifeanyi@users.noreply.github.com>
2019-10-20 12:06:10 +0100

    Add files via upload

---
+++ vue.js
@@ -0,0 +1,6 @@
+var todolist = {
+    todo: ['sami', 'son', 5, 'is', 'busy'],
+    todos: ['you', 'no', 'get', 'sense']
+};
+console.log(todolist);
+document.getElementById('demo').innerHTML = todolist.todo;
